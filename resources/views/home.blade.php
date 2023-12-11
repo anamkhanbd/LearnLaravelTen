9f8d6fae7b3359e3b5f046e6e84a779c420524ec
A H ANAM <anamkhanbd2@gmail.com>
2023-12-11 17:44:46 +0600

    home section is a completed #hero

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,172 @@
     Product Pages 
 @endsection
 @section('main.section')
-    
+    <div class="container-fluid" id="index-body">
+        <div class="row">
+            <div class="container">
+                <div class="row">
+                    <div class="col-8">
+                        <div class="row" id="hero-content">
+                            <h1>
+                                <a href="/" class="typewrite" data-period="2000" data-type='[ "RimonIT Solution.", "Website and Software", "Build And sell", "Digital Technology Product." ]'>
+                                  <span class="wrap"></span>
+                                </a>
+                              </h1>
+                        </div>
+                        <div class="row">
+                            <a href="#" class="btn btn-danger hero-learnmore">Learn more</a>
+                            <a href="/contact" class="btn btn-success hero-learnmore">Contct us </a>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <a href="https://www.youtube.com/@RimonITSolution" target="_blank">
+                            <img src="/images/play_button_animation.gif" alt="" class="hero-play">
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    {{--Service Section--}}
+    <div class="container-fluid" id="service">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="slideshow-container">
+
+                        <div class="mySlides fade">
+                            <div class="container-fluid">
+                                <div class="row">
+                                    <div class="col-4">
+                                        <div class="card">
+                                            <img class="card-img-top card-icon" src="/images/education_cap_graduate_award_icon_149711.png" alt="">
+                                            <div class="card-body">
+                                                <h4 class="card-title">Education Website</h4>
+                                                <p class="card-text">
+                                                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quos aut, nostrum ipsum id molestiae voluptate, enim sapiente tempora, ipsa eum non ab nam voluptas?
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-4">
+                                        <div class="card">
+                                            <img class="card-img-top card-icon" src="/images/frontend-development.png" alt="">
+                                            <div class="card-body">
+                                                <h4 class="card-title">Frontend Development</h4>
+                                                <p class="card-text">
+                                                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quos aut, nostrum ipsum id molestiae voluptate, enim sapiente tempora, ipsa eum non ab nam voluptas?
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-4">
+                                        <div class="card">
+                                            <img class="card-img-top card-icon" src="/images/backend-development.png" alt="">
+                                            <div class="card-body">
+                                                <h4 class="card-title">Backend Development</h4>
+                                                <p class="card-text">
+                                                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quos aut, nostrum ipsum id molestiae voluptate, enim sapiente tempora, ipsa eum non ab nam voluptas?
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div class="mySlides fade">
+                            <div class="container-fluid">
+                                <div class="row">
+                                    <div class="col-4">
+                                        <div class="card">
+                                            <img class="card-img-top card-icon" src="/images/education_cap_graduate_award_icon_149711.png" alt="">
+                                            <div class="card-body">
+                                                <h4 class="card-title">Education Website</h4>
+                                                <p class="card-text">
+                                                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quos aut, nostrum ipsum id molestiae voluptate, enim sapiente tempora, ipsa eum non ab nam voluptas?
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-4">
+                                        <div class="card">
+                                            <img class="card-img-top card-icon" src="/images/frontend-development.png" alt="">
+                                            <div class="card-body">
+                                                <h4 class="card-title">Frontend Development</h4>
+                                                <p class="card-text">
+                                                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quos aut, nostrum ipsum id molestiae voluptate, enim sapiente tempora, ipsa eum non ab nam voluptas?
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-4">
+                                        <div class="card">
+                                            <img class="card-img-top card-icon" src="/images/backend-development.png" alt="">
+                                            <div class="card-body">
+                                                <h4 class="card-title">Backend Development</h4>
+                                                <p class="card-text">
+                                                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quos aut, nostrum ipsum id molestiae voluptate, enim sapiente tempora, ipsa eum non ab nam voluptas?
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div class="mySlides fade">
+                            <div class="container-fluid">
+                                <div class="row">
+                                    <div class="col-4">
+                                        <div class="card">
+                                            <img class="card-img-top card-icon" src="/images/education_cap_graduate_award_icon_149711.png" alt="">
+                                            <div class="card-body">
+                                                <h4 class="card-title">Education Website</h4>
+                                                <p class="card-text">
+                                                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quos aut, nostrum ipsum id molestiae voluptate, enim sapiente tempora, ipsa eum non ab nam voluptas?
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-4">
+                                        <div class="card">
+                                            <img class="card-img-top card-icon" src="/images/frontend-development.png" alt="">
+                                            <div class="card-body">
+                                                <h4 class="card-title">Frontend Development</h4>
+                                                <p class="card-text">
+                                                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quos aut, nostrum ipsum id molestiae voluptate, enim sapiente tempora, ipsa eum non ab nam voluptas?
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-4">
+                                        <div class="card">
+                                            <img class="card-img-top card-icon" src="/images/backend-development.png" alt="">
+                                            <div class="card-body">
+                                                <h4 class="card-title">Backend Development</h4>
+                                                <p class="card-text">
+                                                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quos aut, nostrum ipsum id molestiae voluptate, enim sapiente tempora, ipsa eum non ab nam voluptas?
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        </div>
+                        <br>
+                        
+                        <div style="text-align:center">
+                          <span class="dot"></span> 
+                          <span class="dot"></span> 
+                          <span class="dot"></span> 
+                        </div>
+                    </div>
+                </div>
+            </div>
+                
+        </div>
+    </div>
 @endsection
